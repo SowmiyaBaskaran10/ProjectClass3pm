32b116ad547f247274afb66b2b1782e2956806b3
SowmiyaBaskaran10 <sowmiyabaskaran1996@gmail.com>
2026-02-18 11:07:45 +0530

    New Project - First Push

--- src/test/java/com/omrbranch/project/sample.java
+++ src/test/java/com/omrbranch/project/sample.java
@@ -1,5 +1,18 @@
 package com.omrbranch.project;
 
 public class sample {
+	
+	private void method01() {
+		System.out.println("method1");
+
+	}
+	private void method2() {
+		System.out.println("method2");
+
+	}
+	private void method3() {
+		System.out.println("method3");
+
+	}
 
 }
