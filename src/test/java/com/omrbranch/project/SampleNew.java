f5ce2950a334b09dc27c3965ca7bf6101d6c21ed
SowmiyaBaskaran10 <sowmiyabaskaran1996@gmail.com>
2026-02-18 12:43:37 +0530

    First Commit

---
+++ src/test/java/com/omrbranch/project/SampleNew.java
@@ -0,0 +1,14 @@
+package com.omrbranch.project;
+
+public class SampleNew {
+	
+	private void Java() {
+		System.out.println("Java");
+
+	}
+	private void python() {
+		System.out.println("python");
+
+	}
+
+}
